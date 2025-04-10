03344e49ee8102b7a0a10a93de204bcfd8b951bc
LouisH <louis.hum@le-campus-numerique.fr>
2025-04-10 15:18:34 +0200

    ajout modèle gemma3

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, current_app
 import os
-
+import ollama
 
 main_bp = Blueprint("main",__name__,template_folder="templates", static_folder="static")
 
@@ -16,4 +16,19 @@ def gallerie():
     img_path = ['../static/img/' + x for x in file_names]
     current_app.logger.info(img_path)
 
-    return render_template("gallerie.html", files_path=img_path)
+
+
+    ollama.pull('gemma3:12b')
+    response = ollama.chat(model='gemma3:12b', 
+    messages=[{
+        'role': 'user', 
+        'content': "Raconte une histoire effrayante en t'inspirant de l'image, l'histoire doit faire 3 phrases.",
+        'images': ["app/static/img/4pote.jpg"]
+    }],
+    # options={"temperature":0.7}
+    )
+
+    return render_template("gallerie.html", files_path=img_path, rep=response)
+
+
+    
