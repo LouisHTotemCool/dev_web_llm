8cd393b2dc00243c19d2bf6e4059b8d9bfb15693
LouisH <louis.hum@le-campus-numerique.fr>
2025-04-10 11:17:48 +0200

    Galerie img dynamique

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
+import os
+
 
 main_bp = Blueprint("main",__name__,template_folder="templates", static_folder="static")
 
@@ -8,4 +10,10 @@ def index():
 
 @main_bp.route("/gallerie")
 def gallerie():
-    return render_template("gallerie.html")
+    file_names = sorted(os.listdir('app/static/img'))
+    current_app.logger.info(file_names)
+    
+    img_path = ['../static/img/' + x for x in file_names]
+    current_app.logger.info(img_path)
+
+    return render_template("gallerie.html", files_path=img_path)
